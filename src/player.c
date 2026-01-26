a1c2c96dcbedc22a524b55408b65c62c0b0aab54
Timothy Tripp <ti.n.tripp@baypath.net>
2026-01-25 23:37:40 -0500

    Moving player!

--- src/player.c
+++ src/player.c
@@ -1,13 +1,24 @@
+#include "raylib.h"
+#include "raymath.h"
 #include "player.h"
+#include <stdio.h>
 
 void player_init(Player *player){
-    player->x = 0;
-    player->y = 0;
-    player->sprite = LoadImage("res/player.png");
-    player->speed = 120;
+    player->sprite = LoadTexture("res/player.png");
+    player->dir = PLRDIR_RIGHT;
+
+    player->anim = PLRANIM_FACEDOWN;
+    player->anim_frame = 0;
+
+    player->velocity = (Vector2){ 0, 0 };
+    player->speed = 50;
+    player->x = 80;
+    player->y = 30;
+    player->w = 16;
+    player->h = 22;
 }
 void player_cleanup(Player *player){
-    UnloadImage(player->sprite);
+    UnloadTexture(player->sprite);
 }
 
 void player_event(Player *player){
@@ -18,10 +29,35 @@ void player_update(Player *player){
         IsKeyDown(KEY_D) - IsKeyDown(KEY_A), 
         IsKeyDown(KEY_S) - IsKeyDown(KEY_W)
     };
+    player->velocity = Vector2Scale(
+        Vector2Normalize(move_vec), player->speed
+    );
 
-    player->x += (move_vec.x) * GetFrameTime() * player->speed;
-    player->y += (move_vec.y) * GetFrameTime() * player->speed;
+    player->anim_frame += Vector2Length(player->velocity) * GetFrameTime() / 8;
+    if (player->velocity.x < 0) player->dir = PLRDIR_LEFT;
+    if (player->velocity.x > 0) player->dir = PLRDIR_RIGHT;
+    if (player->velocity.y < 0) player->anim = PLRANIM_FACEUP;
+    if (player->velocity.y > 0) player->anim = PLRANIM_FACEDOWN;
+
+    player->x += (player->velocity.x) * GetFrameTime();
+    player->y += (player->velocity.y) * GetFrameTime();
 }
 void player_draw(Player *player){
-    DrawRectangle(player->x, player->y, 64, 64, PINK);
+    //DrawRectangle(player->x, player->y, player->w, player->h, PINK);
+
+    if (!Vector2Length(player->velocity)){
+        player->anim_frame = 0;
+    }
+    //soon 2 will be anim length -- make json parsing work
+    int frame = ((int)player->anim_frame % 2) + player->anim; 
+
+    DrawTextureRec(
+        player->sprite, 
+        (Rectangle){
+            player->w * frame, 0,
+            player->w * player->dir, player->h
+        }, 
+        (Vector2){ player->x, player->y, }, 
+        WHITE
+    );
 }
--- src/player.h
+++ src/player.h
@@ -1,11 +1,29 @@
 #pragma once
 #include "raylib.h"
 
+typedef enum PlayerDirection {
+    PLRDIR_RIGHT = 1,
+    PLRDIR_LEFT = -1
+} PlayerDirection;
+
+typedef enum PlayerAnim { // int offsets to anims in spritesheet
+    PLRANIM_FACEDOWN = 0,
+    PLRANIM_FACEUP = 2
+} PlayerAnim;
+
 typedef struct Player Player;
 
 struct Player{
-    int x, y, speed;
-    Image sprite;
+    Texture sprite;
+    Vector2 velocity;
+
+    PlayerDirection dir;
+    PlayerAnim anim;
+    float anim_frame;
+
+    float speed;
+    float x, y;
+    int w, h;
 };
 
 void player_init(Player *player);
